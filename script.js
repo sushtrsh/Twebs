416e36c060e4ab958cee2347045eb5b88c97b1b1
v.stasiuk <vladbarsyk221999@gmail.com>
2024-08-16 00:02:23 +0400

    v1.2.13 - add map "2024-08-16"

--- script.js
+++ script.js
@@ -455,6 +455,24 @@ const maps = {
         ],
         key: {id: 12, x: 0, y: 2, width: 2, height: 1, direction: 'horizontal'},
         exitPosition: {x: 6, y: 2}
+    },
+    '2024-08-16': {
+        blocks: [
+            {id: 1, x: 0, y: 4, width: 1, height: 2, direction: 'vertical', color: 'red'},
+            {id: 2, x: 2, y: 1, width: 1, height: 2, direction: 'vertical', color: 'red'},
+            {id: 3, x: 3, y: 2, width: 1, height: 2, direction: 'vertical', color: 'red'},
+            {id: 4, x: 4, y: 2, width: 1, height: 3, direction: 'vertical', color: 'red'},
+            {id: 5, x: 5, y: 0, width: 1, height: 2, direction: 'vertical', color: 'red'},
+            {id: 6, x: 5, y: 4, width: 1, height: 2, direction: 'vertical', color: 'red'},
+            {id: 7, x: 1, y: 0, width: 3, height: 1, direction: 'horizontal', color: 'green'},
+            {id: 8, x: 3, y: 1, width: 2, height: 1, direction: 'horizontal', color: 'green'},
+            {id: 9, x: 3, y: 5, width: 2, height: 1, direction: 'horizontal', color: 'green'},
+            {id: 10, x: 0, y: 3, width: 3, height: 1, direction: 'horizontal', color: 'green'},
+            {id: 11, x: 1, y: 4, width: 3, height: 1, direction: 'horizontal', color: 'green'},
+            {id: 12, x: 0, y: 2, width: 2, height: 1, direction: 'horizontal', color: 'blue'},
+        ],
+        key: {id: 12, x: 0, y: 2, width: 2, height: 1, direction: 'horizontal'},
+        exitPosition: {x: 6, y: 2}
     }
 };
 
@@ -691,7 +709,7 @@ function startCountdown(targetDate) {
     const interval = setInterval(updateCountdown, 1000);
 }
 
-const targetDate = new Date('Sun Aug 15 2024 20:30:00 GMT+0000').getTime();
+const targetDate = new Date('Sun Aug 16 2024 20:30:00 GMT+0000').getTime();
 const beforeFourHours = new Date(targetDate - 4 * 60 * 60 * 1000).getTime();
 const nowTime = new Date().getTime();
 
@@ -700,7 +718,7 @@ if (targetDate > nowTime && beforeFourHours < nowTime) {
 }
 
 const dateParam = getQueryParam('date');
-const date = '2024-08-15';
+const date = '2024-08-16';
 
 const dateDisplay = document.getElementById('date-display');
 dateDisplay.textContent = date;
