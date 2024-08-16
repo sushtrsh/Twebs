9132a7204cc45014aee639fcc70153eadf71b4fd
v.stasiuk <vladbarsyk221999@gmail.com>
2024-08-16 22:22:49 +0400

    v1.2.15 - add copy button

--- keys/keys.js
+++ keys/keys.js
@@ -3,31 +3,37 @@ const games = {
         name: 'Riding Extreme 3D',
         appToken: 'd28721be-fd2d-4b45-869e-9f253b554e50',
         promoId: '43e35910-c168-4634-ad4f-52fd764a843f',
+        interval: 20,
     },
     2: {
         name: 'Chain Cube 2048',
         appToken: 'd1690a07-3780-4068-810f-9b5bbf2931b2',
         promoId: 'b4170868-cef0-424f-8eb9-be0622e8e8e3',
+        interval: 20
     },
     3: {
         name: 'My Clone Army',
         appToken: '74ee0b5b-775e-4bee-974f-63e7f4d5bacb',
         promoId: 'fe693b26-b342-4159-8808-15e3ff7f8767',
+        interval: 121
     },
     4: {
         name: 'Train Miner',
         appToken: '82647f43-3f87-402d-88dd-09a90025313f',
         promoId: 'c4480ac7-e178-4973-8061-9ed5b2e17954',
+        interval: 121,
     },
     5: {
         name: 'Merge Away',
         appToken: '8d1cc2ad-e097-4b86-90ef-7a27e19fb833',
         promoId: 'dc128d28-c45b-411c-98ff-ac7726fbaea4',
+        interval: 21,
     },
     6 : {
         name: 'TwerkRace',
         appToken: '61308365-9d16-4040-8bb0-2f4a4c69074c',
         promoId: '61308365-9d16-4040-8bb0-2f4a4c69074c',
+        interval: 20,
     }
 };
 
@@ -77,7 +83,7 @@ async function loginClient(gameNumber) {
 }
 
 async function registerEvent(token, gameNumber) {
-    await new Promise(resolve => setTimeout(resolve, 20000));
+    await new Promise(resolve => setTimeout(resolve, games[gameNumber].interval * 1000));
     const eventId = generateRandomUUID();
     const url = 'https://api.gamepromo.io/promo/register-event';
     const data = {
@@ -224,6 +230,24 @@ function startProcessGeneration(generationTime) {
     }
 
     updateProcessGenerationTime();
+}
+
+async function copyCode(codeId, button) {
+    try {
+        const content = document.getElementById(codeId).textContent;
+        await navigator.clipboard.writeText(content);
+        
+        const toast = document.getElementById('toast');
+
+        const buttonRect = button.getBoundingClientRect();
+        toast.style.left = `${buttonRect.left + window.scrollX - 10}px`;
+        toast.style.top = `${buttonRect.top - toast.offsetHeight + window.scrollY}px`;
+
+        toast.classList.add('show');
+        setTimeout(() => toast.classList.remove('show'), 2000); // Hide after 2 seconds
+    } catch (error) {
+        console.error('Failed to copy content: ', error);
+    }
 }
 
 generateButton.addEventListener('click', generate);
