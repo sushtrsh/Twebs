8f2d1fb7225aecb9fcd604643033bc111a68d212
v.stasiuk <vladbarsyk221999@gmail.com>
2024-09-13 00:04:29 +0400

    v1.2.57 - add map "2024-09-13"

--- script.js
+++ script.js
@@ -947,6 +947,23 @@ const maps = {
         ],
         key: {id: 12, x: 0, y: 2, width: 2, height: 1, direction: 'horizontal'},
         exitPosition: {x: 6, y: 2}
+    },
+    '2024-09-13': {
+        blocks: [
+            {id: 1, x: 0, y: 3, width: 1, height: 3, direction: 'vertical', color: 'red'},
+            {id: 2, x: 1, y: 4, width: 1, height: 2, direction: 'vertical', color: 'red'},
+            {id: 3, x: 2, y: 1, width: 1, height: 2, direction: 'vertical', color: 'red'},
+            {id: 4, x: 3, y: 1, width: 1, height: 2, direction: 'vertical', color: 'red'},
+            {id: 5, x: 5, y: 3, width: 1, height: 2, direction: 'vertical', color: 'red'},
+            {id: 6, x: 4, y: 2, width: 1, height: 3, direction: 'vertical', color: 'red'},
+            {id: 7, x: 4, y: 1, width: 2, height: 1, direction: 'horizontal', color: 'green'},
+            {id: 8, x: 2, y: 4, width: 2, height: 1, direction: 'horizontal', color: 'green'},
+            {id: 9, x: 4, y: 5, width: 2, height: 1, direction: 'horizontal', color: 'green'},
+            {id: 10, x: 2, y: 0, width: 3, height: 1, direction: 'horizontal', color: 'green'},
+            {id: 11, x: 0, y: 2, width: 2, height: 1, direction: 'horizontal', color: 'blue'},
+        ],
+        key: {id: 11, x: 0, y: 2, width: 2, height: 1, direction: 'horizontal'},
+        exitPosition: {x: 6, y: 2}
     }
 };
 
@@ -1183,7 +1200,7 @@ function startCountdown(targetDate) {
     const interval = setInterval(updateCountdown, 1000);
 }
 
-const targetDate = new Date('Sun Sep 12 2024 20:30:00 GMT+0000').getTime();
+const targetDate = new Date('Sun Sep 13 2024 20:30:00 GMT+0000').getTime();
 const beforeFourHours = new Date(targetDate - 4 * 60 * 60 * 1000).getTime();
 const nowTime = new Date().getTime();
 
@@ -1192,7 +1209,7 @@ if (targetDate > nowTime && beforeFourHours < nowTime) {
 }
 
 const dateParam = getQueryParam('date');
-const date = '2024-09-12';
+const date = '2024-09-13';
 
 const dateDisplay = document.getElementById('date-display');
 dateDisplay.textContent = date;
